f6ed30a441ee9af58c2b5b5b3147378fe5d5b1dd
mwaney <emwaniki91@gmail.com>
2023-07-19 00:53:07 +0300

    Add pictures and clean up dbConnection

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
+const port = process.env.PORT || 3000;
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+const dbURI = `mongodb://localhost/vault`;
 
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    app.listen(port, () => console.log("App listening on port: ", port));
+    console.log("Connected to MongoDB...");
+  } catch (err) {
+    throw new Error("Error connecting to MongoDB:", err.message);
+  }
+};
+dbConnection();
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
