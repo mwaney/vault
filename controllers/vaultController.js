948e7b406efdc7cf7538085f6d95e8f4e232c6a2
mwaney <emwaniki91@gmail.com>
2023-07-19 12:30:29 +0300

    create logic for creating new user, handle errors, and hash password

--- controllers/vaultController.js
+++ controllers/vaultController.js
@@ -1,14 +1,43 @@
+const User = require("../models/User");
+
+const errorHandler = (err) => {
+  console.log(err.message, err.code);
+  const errors = { email: "", password: "" };
+
+  // duplicate error
+  if (err.code === 11000) {
+    errors.email = "This email is already registered";
+    return errors;
+  }
+  if (err.message.includes("User validation failed")) {
+    // validation errors
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
 const signup_get = (req, res) => {
   res.render("signup");
 };
-const signup_post = (req, res) => {
-  res.send("new Sign Up");
+const signup_post = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.create({
+      email,
+      password,
+    });
+    res.status(201).send(user);
+  } catch (err) {
+    const errors = errorHandler(err);
+    res.status(400).send(errors);
+  }
 };
 
 const login_get = (req, res) => {
   res.render("login");
 };
-const login_post = (req, res) => {
+const login_post = async (req, res) => {
   res.send("User Login");
 };
 
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please enter an email address"],
+    lowercase: true,
+    unique: true,
+    validate: [isEmail, "Please Enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+  },
+});
+
+// Fire a function before doc has been saved to db
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
